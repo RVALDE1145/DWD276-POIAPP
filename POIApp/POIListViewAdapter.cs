0c690d39fd95fef92a452f9c0e2bea625044d2a9
RVALDE1145 <RVALDE1145@WS-AC-CTD-37010>
2016-10-26 21:34:45 -0400

    Add project files.

---
+++ POIApp/POIListViewAdapter.cs
@@ -0,0 +1,85 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+
+namespace POIApp
+{
+    public class POIListViewAdapter : BaseAdapter<PointOfInterest>
+    {
+        private readonly Activity context;
+        private List<PointOfInterest> poiListData;
+
+        public POIListViewAdapter(Activity _context, List<PointOfInterest> _poiListData) : base()
+        {
+            this.context = _context;
+            this.poiListData = _poiListData;
+
+        }
+
+        public override int Count
+        {
+            get
+            {
+                return poiListData.Count;
+            }
+        }
+
+        public override long GetItemId(int position)
+        {
+            return position;
+        }
+
+        public override PointOfInterest this[int index]
+        {
+            get
+            {
+                return poiListData[index];
+            }
+        }
+
+        public override View GetView(int position, View convertView, ViewGroup parent)
+        {
+            var view = convertView;
+            if(view==null)
+            {
+                view = context.LayoutInflater.Inflate(Resource.Layout.POIListItem, null);
+            }
+
+            // Get poi data
+            PointOfInterest poi = this[position];
+
+            // set the text of the nameTextView to the POI's name
+            view.FindViewById<TextView>(Resource.Id.nameTextView).Text = poi.Name;
+
+            if(string.IsNullOrEmpty(poi.Address))
+            {
+                view.FindViewById<TextView>(Resource.Id.addrTextView).Visibility = ViewStates.Gone;
+            }
+            else
+            {
+                view.FindViewById<TextView>(Resource.Id.addrTextView).Text = poi.Address;
+            }
+
+            // get reference to imageview
+            var imageView = view.FindViewById<ImageView>(Resource.Id.poiImageView);
+
+            if(!String.IsNullOrEmpty(poi.Image))
+            {
+                Koush.UrlImageViewHelper.SetUrlDrawable(imageView, poi.Image, Resource.Drawable.ic_launcher);
+            }
+
+            return view;
+
+
+        }
+    }
+}
